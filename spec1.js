c74cfeb93d9abe9f30cce60df7a5f0547a906b8f
Adrian J Lopez <206582563>
2019-02-07 17:49:51 -0500

    Reorganization of two tests in spec1,js.

--- spec1.js
+++ spec1.js
@@ -1,20 +1,19 @@
+'use strict';
 describe('Protractor start', () => {
 	it('Open angular website', () => {
 		//load webpages
 		browser.get("https://angularjs.org");
+	});
+	it('open website and wait 5 seconds before closing', function() {
 		browser.get('http://juliemr.github.io/protractor-demo')
 		//after page is loaded, wait for promise to resolve
 		.then(() => {
 			//wait for 5 seconds on page
 			browser.sleep(5000);
 		//after browser has slept for 5 seconds, wait for promise to resolve
-		.then(() => {
+		}).then(() => {
 			//log that we are finished
 			console.log('finished');
 		});
-		
-	});
-	it('close browser', function() {
-
 	});
 });
