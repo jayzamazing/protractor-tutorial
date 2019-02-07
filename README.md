ef3ea45eed2d328177fd7c23a90ea32dd78107d9
Adrian J Lopez <206582563>
2019-02-07 15:21:03 -0500

    Addition of another webpage to open angular test and promise to wait till it is loaded.

--- README.md
+++ README.md
@@ -7,5 +7,4 @@ This project is just a play ground for me to be able to play with protractor and
 > For now this section assumes that you have installed protractor globally. Basic instructions on how to install protractor will follow later.
 
 1. In a terminal window type in "webdriver-manager start".
-2. In a separate terminal window type "protractor conf.js
-"
+2. In a separate terminal window type "protractor conf.js"
--- spec1.js
+++ spec1.js
@@ -1,6 +1,18 @@
-describe('Protractor start', function() {
-	it('Open angular website', function() {
-
+describe('Protractor start', () => {
+	it('Open angular website', () => {
+		//load webpages
+		browser.get("https://angularjs.org");
+		browser.get('http://juliemr.github.io/protractor-demo')
+		//after page is loaded, wait for promise to resolve
+		.then(() => {
+			//wait for 5 seconds on page
+			browser.sleep(5000);
+		//after browser has slept for 5 seconds, wait for promise to resolve
+		.then(() => {
+			//log that we are finished
+			console.log('finished');
+		});
+		
 	});
 	it('close browser', function() {
 
